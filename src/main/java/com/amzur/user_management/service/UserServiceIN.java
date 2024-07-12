7076072a55ce77a71fee89320033817678555454
DURGASI MAHESH KUMAR <124244425+Mahesh-100@users.noreply.github.com>
2024-07-12 19:45:55 +0530

    updated user management

--- src/main/java/com/amzur/user_management/service/UserServiceIN.java
+++ src/main/java/com/amzur/user_management/service/UserServiceIN.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -21,8 +22,9 @@ import com.amzur.user_management.entities.UserEntity;
 import com.amzur.user_management.handlers.ResourceNotAvailable;
 import com.amzur.user_management.handlers.UserAlreadyExist;
 import com.amzur.user_management.repository.UserRepository;
+@Profile("in")
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceIN implements UserService{
 
 	
 	private UserRepository userRepository;
@@ -31,7 +33,7 @@ public class UserServiceImpl implements UserService{
 	
 
      @Autowired                                                              
-	public UserServiceImpl(UserRepository userRepository, RestTemplate restTemplate) {
+	public UserServiceIN(UserRepository userRepository, RestTemplate restTemplate) {
 		super();
 		this.userRepository = userRepository;
 		this.restTemplate = restTemplate;
---
+++ src/main/java/com/amzur/user_management/service/UserServiceUS.java
@@ -0,0 +1,149 @@
+
+package com.amzur.user_management.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.amzur.user_management.constants.ApplicationConstants;
+import com.amzur.user_management.dto.request.UserRequest;
+import com.amzur.user_management.dto.response.OrderResponse;
+import com.amzur.user_management.dto.response.UserResponse;
+import com.amzur.user_management.entities.UserEntity;
+import com.amzur.user_management.handlers.ResourceNotAvailable;
+import com.amzur.user_management.handlers.UserAlreadyExist;
+import com.amzur.user_management.repository.UserRepository;
+@Profile("us")
+@Service
+public class UserServiceUS implements UserService{
+
+	
+	private UserRepository userRepository;
+	private final RestTemplate restTemplate;
+	
+	
+
+     @Autowired                                                              
+	public UserServiceUS(UserRepository userRepository, RestTemplate restTemplate) {
+		super();
+		this.userRepository = userRepository;
+		this.restTemplate = restTemplate;
+	}
+
+	@Override
+	public UserResponse save(UserRequest userRequest) {
+		 Optional<UserEntity> existingUser = userRepository.findByEmailAndPassword(userRequest.getEmail(), userRequest.getPassword());
+		 
+		 if(existingUser.isPresent()) {
+			 throw new UserAlreadyExist(ApplicationConstants.USER_ALREADY_EXIST);
+		 }else {
+			 UserEntity userEntity=new UserEntity();
+				BeanUtils.copyProperties(userRequest, userEntity);
+				userEntity=userRepository.save(userEntity);
+				 return convertEntityToResponse(userEntity);
+		 }
+		
+		
+//		UserResponse userResponse=new UserResponse();
+//		BeanUtils.copyProperties(userEntity, userResponse);
+	 
+		
+	}
+
+	@Override
+	public UserResponse findByEmail(String email,String password) {
+		UserEntity userEntity=userRepository.findByEmailAndPassword(email,password).orElseThrow(()->new ResourceNotAvailable(ApplicationConstants.USER_NOT_FOUND));
+				
+		return convertEntityToResponse(userEntity);
+		
+		//UserResponse userResponse=new UserResponse();
+		
+		
+		// by using setters and getters method
+//		userResponse.setUserId(user.getUserId());
+//		userResponse.setName(user.getName());
+//		userResponse.setPhoneNumber(user.getPhoneNumber());
+//		userResponse.setAddress(user.getAddress());
+//		userResponse.setEmail(user.getEmail());
+		
+		
+		//BeanUtils.copyProperties(userEntity, userResponse);
+	
+	}
+
+	@Override
+	public List<UserResponse> findAll() {
+//		List<UserEntity> users=userRepository.findAll();
+//		List<UserResponse>userResponses=new ArrayList<UserResponse>();
+//		for(UserEntity userEntity:users) {
+////			UserResponse userReponse=new UserResponse();
+////			BeanUtils.copyProperties(userEntity, userReponse);
+//			userResponses.add(convertEntityToResponse(userEntity));
+//		}
+		return userRepository.findAll().stream().map(this::convertEntityToResponse).collect(Collectors.toList()
+				);
+	}
+
+	@Override
+	public void deleteById(long userId) {
+		  UserResponse userResponse=findById(userId);
+		
+		userRepository.deleteById(userResponse.getUserId());
+
+	}
+	@Override
+	public UserResponse findById(Long id) {
+		UserEntity userEntity=userRepository.findById(id).orElseThrow(()->new  ResourceNotAvailable(ApplicationConstants.RESOURCE_NOT_FOUND));
+//		if(userEntityopt.isPresent()) {
+//			userEntity=userEntityopt.get();
+//			return convertEntityToResponse(userEntity);
+//		}else {
+//			throw new ResourceNotAvailable("User Not Found");
+//		}
+		return convertEntityToResponse(userEntity);
+	
+	}
+	
+	public List<OrderResponse> getUserOrders(String email, String password) {
+        UserResponse userResponse = findByEmail(email, password);
+        
+        	
+            String url = "http://localhost:8080/orders/userId/" + userResponse.getUserId();
+            
+            ResponseEntity<List<OrderResponse>> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<OrderResponse>>() {}
+            );
+            
+            return response.getBody();
+        
+    }
+	
+
+	public UserResponse convertEntityToResponse(UserEntity userEntity) {
+		UserResponse userReponse=new UserResponse();
+		BeanUtils.copyProperties(userEntity, userReponse);
+		return userReponse;
+	}
+
+	
+
+	
+
+	
+	
+
+	
+	
+}
