33366288afcb4cc25e86d067c2cfc327edfe6a17
DURGASI MAHESH KUMAR <124244425+Mahesh-100@users.noreply.github.com>
2024-07-23 15:44:28 +0530

    CORS added

--- src/main/java/com/amzur/user_management/controller/UserController.java
+++ src/main/java/com/amzur/user_management/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.amzur.user_management.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,10 +19,12 @@ import com.amzur.user_management.dto.request.UserRequest;
 import com.amzur.user_management.dto.response.OrderResponse;
 import com.amzur.user_management.dto.response.UserResponse;
 import com.amzur.user_management.service.UserService;
+
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
 	@Autowired
@@ -40,8 +43,8 @@ public class UserController {
 		
 	}
 	@PostMapping("/login")
-	public List<OrderResponse> getUserDetails(@RequestParam("email") String email, @RequestParam("password") String password ) {
-		return userService.getUserOrders(email,password);
+	public List<OrderResponse> getUserDetails(@RequestBody UserRequest userRequest ) {
+		return userService.getUserOrders(userRequest.getEmail(),userRequest.getPassword());
 		
 	}
 	
